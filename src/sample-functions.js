984c4a823725b4c77bcb0ea755af57229164561c
Anamooz <145793103+Anamooz@users.noreply.github.com>
2024-01-25 14:37:08 -0800

    finished testing

--- src/sample-functions.js
+++ src/sample-functions.js
@@ -10,5 +10,24 @@ function div (a, b){
     return false;
   }
 
+  function sum(a,b) {
+    if(!typeIsNumber(a) || !typeIsNumber(b)){
+      throw new TypeError('Must be a number', "sample-functions.js");
+    } else {
+      return a + b;
+    }
+  }
+
+  
+  function typeIsNumber(a){
+    if (isNaN(a)){
+      return false;
+    }
+    return(typeof(a) === 'number');
+  }
+  
+
 exports.div = div;
 exports.containsNumbers = containsNumbers;
+exports.sum = sum;
+exports.typeIsNumber = typeIsNumber;
