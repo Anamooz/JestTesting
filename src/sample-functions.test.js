c713c0d652be058d5ceaa920034142fb2182ebd1
Anamooz <145793103+Anamooz@users.noreply.github.com>
2024-01-27 11:21:40 -0800

    init commit

--- src/sample-functions.test.js
+++ src/sample-functions.test.js
@@ -5,3 +5,40 @@ test('Testing sum -- success', () => {
   const result = myFunctions.sum(12, 18);
   expect(target).toBe(result);
 });
+
+test('Testing a negative number -- success', () => {
+  const target = 6;
+  const result = myFunctions.sum(-12, 18);
+  expect(target).toBe(result);
+});
+
+test('Testing negative numbers -- success', () => {
+  const target = -30;
+  const result = myFunctions.sum(-12, -18);
+  expect(target).toBe(result);
+});
+
+test('Testing div numbers -- success', () => {
+  const target = 5;
+  const result = myFunctions.div(10, 2);
+  expect(target).toBe(result);
+});
+
+test('Testing negative div numbers -- success', () => {
+  const target = -5;
+  const result = myFunctions.div(10, -2);
+  expect(target).toBe(result);
+});
+
+test('Testing contain numbers -- success', () => {
+  const target = false;
+  const result = myFunctions.containsNumbers("therearenonumbers");
+  expect(target).toBe(result);
+});
+
+test('Testing no contain numbers -- success', () => {
+  const target = true;
+  const result = myFunctions.containsNumbers("yesnumebrs20398203");
+  expect(target).toBe(result);
+});
+
